c302d9abfa54b0a0a779d641a47497d7499e5efa
Nickysukka72922 <Nicky.sukkar@gmail.com>
2026-03-10 21:13:41 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-
+let cloneCount = 0;
+function addtask() {
+ const original = document.getElementById("task");
+    const clone = original.cloneNode(true);
+cloneCount++;
+    clone.id = "task-" + cloneCount;
+document.getElementById("body").appendChild(clone);
+}
